206572444627e5cfb48d6449f8dc45d3af2966b1
danieelaugustoo <aasilva.daniel@gmail.com>
2025-08-08 10:14:28 -0300

    commit

---
+++ exercicios/ex007/index.php
@@ -0,0 +1,16 @@
+<?php 
+    $velocidade = 800;
+    if ($velocidade >  100 ) {
+        echo "Muito rápido"; }
+
+        elseif ($vlelocidade > 60 ) {
+        echo "Muito rápido";
+    }
+        elseif ($velocidade > 80){
+            echo "Acima do limite";
+        }
+    else {
+        echo "Lento demais";
+    }
+
+?>
